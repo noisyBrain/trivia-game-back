298c6342edd9acf02c93cc8c3484b736969cfd97
Tomas <tomasarreguib@gmail.com>
2022-09-05 21:44:21 -0300

    add: connection to db

---
+++ src/db.ts
@@ -0,0 +1,5 @@
+import { Sequelize } from 'sequelize'
+import 'dotenv/config';
+
+const URI = `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:5432/${process.env.DATABASE_NAME}`
+export const db = new Sequelize(URI)
